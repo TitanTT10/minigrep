0e97e9bd8a485135271ce14ee761e61a21b1b0e8
TitanTT10 <titan@tenwinkel.org>
2025-09-26 23:20:18 +0200

    Some of the changes from ch12-03

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = std::fs::read_to_string(file_path)
+    run(config);
+}
+
+fn run(config: Config) {
+    let contents = std::fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 { return Err("not enough arguments"); }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Self { query, file_path })
+    }
 }
